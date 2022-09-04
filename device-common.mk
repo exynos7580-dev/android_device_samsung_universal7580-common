f2d8cb34d4670093e633a241d66e00335ca5a53d
Eyüp DURAK <dkey5@pm.me>
2022-09-04 12:40:21 +0300

    universal7580-common: adapt for PixelExperience

--- device-common.mk
+++ device-common.mk
@@ -183,10 +183,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.samsung
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
